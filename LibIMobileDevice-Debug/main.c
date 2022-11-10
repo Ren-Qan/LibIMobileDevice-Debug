cd468970e0c485e55649624d429dc99f6b9b8548
RYQ <1048040579@qq.com>
2022-11-10 21:14:53 +0800

    feature: first commit

---
+++ LibIMobileDevice-Debug/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LibIMobileDevice-Debug
+//
+//  Created by 任玉乾 on 2022/11/10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
