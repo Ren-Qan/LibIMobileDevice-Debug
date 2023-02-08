1e853a97936b09bbd78ae8adaf759efabbd1afce
RYQ <1048040579@qq.com>
2023-02-08 11:13:05 +0800

    feature: test code

--- libimobiledevice/main.cc
+++ libimobiledevice/main.cc
@@ -11,16 +11,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "mobilebackup2.h"
 #include "libimobiledevice.h"
-#include "DTXMessageService.hh"
-
-static void instruments_call_back(uint32_t channel, void * object) {
-    printf("=======%d\n", channel);
-}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-//    idevice_set_debug_level(1);
     char ** list = NULL;
     int count = 0;
     
@@ -28,60 +22,39 @@ int main(int argc, const char * argv[]) {
     
     if (state == IDEVICE_E_SUCCESS && count > 0) {
         for (int i = 0; i < count; i++) {
-            printf("[UUID] - [%s]\n", list[i]);
+            printf("[ID] - [%s]\n", list[i]);
         }
     } else {
         printf("\n[ERROR] - [%d]\n", state);
     }
     
+    if (count < 1) {
+        printf("\n device not found");
+        return -1;
+    }
+    
     // creat Device
     idevice_t device = NULL;
-    idevice_new_with_options(&device, list[0], IDEVICE_LOOKUP_NETWORK);
+    idevice_new(&device, list[0]);
 
-    idevice_connection_t conn;
-    instruments_start_connection(device, &conn, instruments_call_back);
+    mobilebackup2_client_t client = NULL;
+    mobilebackup2_error_t error = mobilebackup2_client_start_service(device, &client, "asd");
     
-    const void *procAttrs_values[] = {CFSTR("memVirtualSize"), CFSTR("cpuUsage"), CFSTR("ctxSwitch"), CFSTR("intWakeups"),CFSTR("physFootprint"), CFSTR("memResidentSize"), CFSTR("memAnon"), CFSTR("pid")};
-    int _bm = 0; // don't suspend the process after starting it
-    int _ur = 1000; // kill the application if it is already running
-    int _cpuUsage = 1; // kill the application if it is already running
-    int _sampleInterval = 1000000000; // kill the application if it is already running
+    if (error == MOBILEBACKUP2_E_SUCCESS) {
+        printf("\n mobilebackup2 success\n");
+    } else {
+        printf("\n mobilebackup2 error %d\n", error);
+    }
     
-    CFNumberRef bm = CFNumberCreate(NULL, kCFNumberSInt32Type, &_bm);
-    CFNumberRef ur = CFNumberCreate(NULL, kCFNumberSInt32Type, &_ur);
-    CFNumberRef cpuUsage = CFNumberCreate(NULL, kCFNumberSInt32Type, &_cpuUsage);
-    CFNumberRef sampleInterval = CFNumberCreate(NULL, kCFNumberSInt32Type, &_sampleInterval);
-    CFArrayRef procAttrs = CFArrayCreate(NULL, procAttrs_values, 8, NULL);
+    if (client) {
+        mobilebackup2_client_free(client);
+    }
     
-    const void *keys[] =
-    {
-        CFSTR("bm"),
-        CFSTR("cpuUsage"),
-        CFSTR("procAttrs"),
-        CFSTR("sampleInterval"),
-        CFSTR("ur")
-    };
-    
-    const void *values[] = { bm, cpuUsage, procAttrs, sampleInterval, ur};
-    
-    CFDictionaryRef dic = CFDictionaryCreate(NULL, keys, values, 5, NULL, NULL);
-    
-    message_aux_t args;
-    args.append_obj(dic);
-    
-    int cpu_channel_code = instrument_make_channel(conn, CFSTR("com.apple.instruments.server.services.sysmontap"));
-    instruments_response(conn, cpu_channel_code, (char *)CFSTR("setConfig:"), &args);
-    instruments_response(conn, cpu_channel_code, (char *)CFSTR("start"), NULL);
-    
-    int i = 0;
-    while (i < 10000) {
-        instrument_receive(conn);
-        i++;
+    if (device) {
+        idevice_free(device);
     }
 
-    instrument_connection_free(conn);
-    idevice_free(device);
-    
+    printf("\n");
     return 0;
 }
 
