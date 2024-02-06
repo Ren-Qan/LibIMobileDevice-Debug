26bf8c60aaca755e03998d07c897068a112ad18f
RYQ <1048040579@qq.com>
2024-02-06 18:06:34 +0800

    style: 优化代码格式。todo: 优化命名。

--- TestAPP/XPC+Adopter.swift
+++ TestAPP/XPC+Adopter.swift
@@ -11,10 +11,12 @@ struct XPC { }
 
 extension XPC {
     class Adopter {
-        func creat(_ message_id: Int, _ payload: Any?, _ need_reply: Bool = false) -> Data {
+        func creat(_ message_id: Int, 
+                   _ parameter: Any?,
+                   _ need_reply: Bool = false) -> Data {
             var flags = XPC.Flags.alwaysSet
-
-            if payload != nil {
+            
+            if parameter != nil {
                 flags = [flags, XPC.Flags.dataPresent]
             }
             
@@ -22,61 +24,65 @@ extension XPC {
                 flags = [flags, XPC.Flags.wantingReply]
             }
             
-            let payload = XPC.Payload(buildXPCObject(payload))
-            let message = XPC.Message(UInt64(message_id), payload)
-            let wrapper = XPC.Wrapper(flags, message)
-            return wrapper.bytes()
-        }
-        
-        func buildXPCObject(_ value: Any?) -> XPCObjectProtocol {
-            guard let value else {
-                return XPC._NULL()
+            return XPC.Wrapper.data { make in
+                make.object(self.buildXPCObject(parameter))
+                make.message(UInt64(message_id))
+                make.flags(flags)
             }
-            
-            if let dic = value as? [String : Any] {
-                let entries: [XPC._Dictionary.Entry] = dic.compactMap { item in
-                    let value = buildXPCObject(item.value)
-                    return .init(key: item.key, value: value)
-                }
-                return XPC._Dictionary(entries)
-            }
-            
-            if let array = value as? [Any] {
-                let entries = array.compactMap { value in
-                   return buildXPCObject(value)
-                }
-                return XPC._Array(entries)
-            }
-            
-            if value is Int || value is Int32 || value is Int64, let value = value as? Int64 {
-                return XPC._Int64(value: value)
-            }
-            
-            if value is UInt || value is UInt32 || value is UInt64, let value = value as? UInt64 {
-                return XPC._UInt64(value: value)
-            }
-            
-            if value is Bool, let value = value as? Bool {
-                return XPC._Bool(value: value ? 1 : 0)
-            }
-            
-            if value is String, let value = value as? String {
-                return XPC._String(value: value)
-            }
-            
-            if value is Data, let value = value as? Data {
-                return XPC._Data(value: value)
-            }
-            
-            if value is CGFloat || value is Double, let value = value as? Double  {
-                return XPC._Double(vlaue: value)
-            }
-            
-            if let value = value as? UUID {
-                return XPC._UUID(value: value)
-            }
-            
-            return XPC._NULL()
         }
     }
 }
+
+extension XPC.Adopter {
+    fileprivate func buildXPCObject(_ value: Any?) -> XPCObjectProtocol {
+        guard let value else {
+            return XPC._NULL()
+        }
+        
+        if let dic = value as? [String : Any] {
+            let entries: [XPC._Dictionary.Entry] = dic.compactMap { item in
+                let value = buildXPCObject(item.value)
+                return .init(key: item.key, value: value)
+            }
+            return XPC._Dictionary(entries)
+        }
+        
+        if let array = value as? [Any] {
+            let entries = array.compactMap { value in
+                return buildXPCObject(value)
+            }
+            return XPC._Array(entries)
+        }
+        
+        if value is Int || value is Int32 || value is Int64, let value = value as? Int64 {
+            return XPC._Int64(value: value)
+        }
+        
+        if value is UInt || value is UInt32 || value is UInt64, let value = value as? UInt64 {
+            return XPC._UInt64(value: value)
+        }
+        
+        if value is Bool, let value = value as? Bool {
+            return XPC._Bool(value: value ? 1 : 0)
+        }
+        
+        if value is String, let value = value as? String {
+            return XPC._String(value: value)
+        }
+        
+        if value is Data, let value = value as? Data {
+            return XPC._Data(value: value)
+        }
+        
+        if value is CGFloat || value is Double, let value = value as? Double  {
+            return XPC._Double(vlaue: value)
+        }
+        
+        if let value = value as? UUID {
+            return XPC._UUID(value: value)
+        }
+        
+        return XPC._NULL()
+    }
+}
+
